e1184a2c043fbeac71ad9a534832a12b4cb4c390
Gondolindrim <alvaro.augusto.volpato@gmail.com>
2021-01-29 11:13:29 -0300

    Initial Type K support

---
+++ keyboards/typek/typek.c
@@ -0,0 +1,26 @@
+/*
+Copyright 2020 Álvaro "Gondolindrim" Volpato  <gondolindrim@acheronproject.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "typek.h"
+
+void keyboard_pre_init_user(void) {
+  // Call the keyboard pre init code.
+
+	palSetLineMode(B11, PAL_MODE_OUTPUT_OPENDRAIN);
+	palSetLineMode(B12, PAL_MODE_OUTPUT_OPENDRAIN);
+	palSetLineMode(B14, PAL_MODE_OUTPUT_OPENDRAIN);
+}
