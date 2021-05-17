64f8ce6d2b4f18dee837dc5c7da3f9788b8bc33f
Gondolindrim <alvaro.augusto.volpato@gmail.com>
2021-05-17 15:11:16 -0300

    Finish default firmware

--- keyboards/viendi8L/keymaps/via/keymap.c
+++ keyboards/viendi8L/keymaps/via/keymap.c
@@ -101,6 +101,7 @@ typedef int color[RGB_PIN_COUNT];
 #define YELLOW COLOR(0,0,1)
 #define PINK COLOR(0,1,0)
 #define WHITE COLOR(0,0,0)
+#define OFF COLOR(1,1,1)
 
 #define STARTUP_COLOR YELLOW
 
@@ -116,6 +117,7 @@ const encoder_mode_t encoder_modes[] = {
 	{ .indicator_color = GREEN, .clockwise_key = KC_MPRV, .counterclockwise_key = KC_MNXT, .clicked_key = KC_MPLY },
 	{ .indicator_color = BLUE, .clockwise_key = KC_WH_U, .counterclockwise_key = KC_WH_D, .clicked_key = KC_BTN1 },
 	{ .indicator_color = PINK, .clockwise_key = ALT_TAB_SWITCH, .counterclockwise_key = ALT_TAB_SWITCH, .clicked_key = ALT_TAB_CLICK}, 
+	// Insert your custom encoder mode here
 };
 
 #define NUM_ENCODER_MODES (sizeof(encoder_modes)/sizeof(encoder_modes[0]))
@@ -125,7 +127,10 @@ void set_indicator_colors(const color target_color){
 	for (pin_count = 0 ; pin_count < RGB_PIN_COUNT ; pin_count++) writePin(rgb_indicators_pins[pin_count], target_color[pin_count]) ;
 }
 
+// This counter is used to track what encoder mode is being used at a certain time
 int encoder_mode_count = 0;
+
+// Board init: RGB indicator iss set to STARTUP_COLOR and the THT indicators are set
 void board_init(void){
 	// Set the RGB indicator pins as open drain and set the startup color
 	for (pin_count = 0 ; pin_count < RGB_PIN_COUNT ; pin_count++){
@@ -148,18 +153,18 @@ void keyboard_post_init_user(void){
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    [0] = LAYOUT_all(
-        ENCODER_CLICK, KC_NLCK, KC_PAST, KC_DEL , KC_ESC , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL , KC_BSPC,
+        ENCODER_CLICK, KC_PSLS, KC_PAST, KC_NLCK, KC_GESC, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL , KC_BSPC,
               KC_P7  , KC_P8  , KC_P9  , KC_PPLS, KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC, KC_BSLS,
               KC_P4  , KC_P5  , KC_P6  , KC_P6  , KC_CAPS, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT, KC_ENT , KC_BSPC,
-	      KC_P1  , KC_P2  , KC_P3  , KC_PENT, KC_LSFT, KC_BSLS, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT, KC_NUHS,
-              KC_P0  , KC_P0  , KC_PDOT, KC_PDOT, KC_LCTL,          KC_LGUI, KC_LALT,                   MO(1),            KC_RCTL, KC_P0  , KC_LEFT, KC_DOWN, KC_RGHT, KC_SLSH
+	      KC_P1  , KC_P2  , KC_P3  , KC_PENT, KC_LSFT, KC_BSLS, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT, MO(1)  ,
+              KC_P0  , KC_P0  , KC_PDOT, KC_PDOT, KC_LCTL,          KC_LGUI, KC_LALT,                   KC_SPC ,          MO(1)  , KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_SLSH
 	),
    [1] = LAYOUT_all(
               ENCDBCH, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
               KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
               KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
               KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS,                   KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, TG(2),            KC_TRNS, KC_TRNS,                   KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
         ),
    [2] = LAYOUT_all(
               KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
@@ -176,7 +181,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
               KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS,                   KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
         )
 };
-
 
 bool is_alt_tab_active = false; // Flag to check if alt tab is active
 uint32_t alt_tab_timer = 0;     // Time trigger for alt tab
@@ -293,7 +297,7 @@ bool led_update_kb(led_t led_state) {
 
 layer_state_t layer_state_set_user(layer_state_t state) {
 	switch (get_highest_layer(state)) {
-		case 1:
+		case 2:
 			writePin(BOT_INDICATOR_PIN, 0);
 			break;
 		default: //  for any other layers, or the default layer
