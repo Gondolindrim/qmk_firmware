b3f5aaeda6f32a798fd77ec4c5d488737a115e75
Gondolindrim <alvaro.augusto.volpato@gmail.com>
2021-06-07 13:06:23 -0300

    Initial layer tapdance implementation

--- keyboards/viendi8L/keymaps/default/keymap.c
+++ keyboards/viendi8L/keymaps/default/keymap.c
@@ -85,12 +85,13 @@ By default, from top to bottom, those indicators are the caps lock indicator, nu
 // Defining encoder click keycode
 enum keyboard_keycodes {
         ENCODER_CLICK = SAFE_RANGE,
-	ENCDBCH, // Stands for ENCODER DELAY BEHAVIOR CHANGE
-	ALTTABS, // For alt-tab-switch
-	ALTTABC, // For alt-tab-click
+	ENCDBCH,// Stands for ENCODER DELAY BEHAVIOR CHANGE
+	ALTTABS,// For alt-tab-switch
+	ALTTABC,// For alt-tab-click
 	ENCMUP, // Encoder mode up
 	ENCMDN, // Encoder mode down
-	TGLCK   // Toggle lock mode
+	TGLCK,  // Toggle lock mode
+	FND     // Toggle layer 2 on tap, turn layer 1 on hold
 };
 
 uint16_t encoder_click_delay = ENCODER_HIGH_DELAY;
@@ -154,36 +155,116 @@ void keyboard_post_init_user(void){
 	for (pin_count = 0 ; pin_count < RGB_PIN_COUNT ; pin_count++) writePin(rgb_indicators_pins[pin_count], encoder_modes[0].indicator_color[pin_count]);
 };
 
+#define TCAPS LT(1, KC_CAPS) // Tap-CAPS configuration: MO(1) when held, CAPS when tapped
+
+// ------------------------------------------------------------------------------------------------
+// Defining the FND key, short for FN DANCE: if held, turns layer 1 on and if tapped toggles layer 2
+// Define a type for as many tap dance states as you need
+typedef enum {
+    TD_NONE,
+    TD_UNKNOWN,
+    TD_SINGLE_TAP,
+    TD_SINGLE_HOLD
+} td_state_t;
+
+typedef struct {
+    bool is_press_action;
+    td_state_t state;
+} td_tap_t;
+
+// Declare the functions to be used with your tap dance key(s)
+
+// Function associated with all tap dances
+td_state_t cur_dance(qk_tap_dance_state_t *state);
+
+// Functions associated with individual tap dances
+void fnd_finished(qk_tap_dance_state_t *state, void *user_data);
+void fnd_reset(qk_tap_dance_state_t *state, void *user_data);
+// ------------------------------------------------------------------------------------------------
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    [0] = LAYOUT_all(
-        ENCODER_CLICK, KC_PSLS, KC_PAST, KC_NLCK, KC_GESC, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL , KC_BSPC,
-              KC_P7  , KC_P8  , KC_P9  , KC_PPLS, KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC, KC_BSLS,
-              KC_P4  , KC_P5  , KC_P6  , KC_P6  , KC_CAPS, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT, KC_ENT , KC_BSPC,
+        ENCODER_CLICK, KC_PSLS, KC_PAST, KC_BSPC, KC_GESC, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL , KC_BSPC,
+              KC_PMNS, KC_P7  , KC_P8  , KC_P9  , KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC, KC_BSLS,
+              KC_PPLS, KC_P4  , KC_P5  , KC_P6  , TCAPS  , KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT, KC_ENT , KC_BSPC,
 	      KC_PENT, KC_P1  , KC_P2  , KC_P3  , KC_LSFT, KC_BSLS, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT, KC_NUHS,
-              KC_P0  , KC_P0  , KC_P0  , KC_PDOT, KC_LCTL,          KC_LGUI, MO(2)  ,                   TGLCK  ,          MO(1)  , KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, MO(1)
+              KC_PENT, KC_P0  , KC_P0  , KC_PDOT, KC_LCTL,          KC_LGUI, KC_LALT,                   KC_SPC ,          TD(FND), KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, MO(1)
 	),
    [1] = LAYOUT_all(
-        ENCODER_CLICK, KC_TRNS, KC_TRNS, KC_TRNS, RESET  , KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, TG(2),            KC_TRNS, KC_TRNS,                   ENCDBCH,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+        ENCODER_CLICK, KC_F10 , KC_F11 , KC_NLCK, KC_GRV , KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS, KC_PLUS, KC_NO  , 
+              KC_MINS, KC_F7  , KC_F8  , KC_F9  , TG(2)  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_INS , KC_NO  , KC_PSCR, KC_NO  , KC_NO  , KC_NO  ,
+              KC_EQL , KC_F4  , KC_F5  , KC_F6  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,
+              KC_NO  , KC_F1  , KC_F2  , KC_F3  , KC_NO  , KC_LPRN, KC_RPRN, KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , 
+              KC_NO  , KC_CALC, KC_CALC, KC_DEL , TGLCK  ,          KC_SLEP, KC_NO  ,                   KC_NO  ,          KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO
         ),
    [2] = LAYOUT_all(
-        ENCODER_CLICK, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS,                   KC_TRNS,          MO(3)  , KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
-        ),
+        ENCODER_CLICK, KC_PSLS, KC_PAST, KC_BSPC, KC_GESC, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , KC_BSPC,
+              KC_PMNS, KC_P7  , KC_P8  , KC_P9  , KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC, KC_BSLS,
+              KC_PPLS, KC_P4  , KC_P5  , KC_P6  , TCAPS  , KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT, KC_ENT , KC_BSPC,
+	      KC_PENT, KC_P1  , KC_P2  , KC_P3  , KC_LSFT, KC_BSLS, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT, KC_NUHS,
+              KC_PENT, KC_P0  , KC_P0  , KC_PDOT, KC_LCTL,          KC_LGUI, KC_LALT,                   KC_SPC ,          TD(FND), KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, MO(1)
+	),
    [3] = LAYOUT_all(
         ENCODER_CLICK, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
               KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
               KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
               KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, TO(0)  ,          KC_TRNS, KC_TRNS,                   KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS,                   KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
         )
 };
+
+// FINISH FN TAPDANCE DEFINITIONS -----------------------------------------------------------------
+// Determine the tapdance state to return
+// Determine the current tap dance state
+td_state_t cur_dance(qk_tap_dance_state_t *state) {
+    if (state->count == 1) {
+        if (!state->pressed) return TD_SINGLE_TAP;
+        else return TD_SINGLE_HOLD;
+    }
+    else return TD_UNKNOWN;
+}
+
+// Initialize tap structure associated with example tap dance key
+static td_tap_t fnd_tap_state = {
+    .is_press_action = true,
+    .state = TD_NONE
+};
+
+// Functions that control what our tap dance key does
+void fnd_finished(qk_tap_dance_state_t *state, void *user_data) {
+    fnd_tap_state.state = cur_dance(state);
+    switch (fnd_tap_state.state) {
+        case TD_SINGLE_HOLD:
+            layer_on(1);
+            break;
+        case TD_SINGLE_TAP:
+            // Check to see if the layer is already set
+            if (layer_state_is(2)) {
+                // If already set, then switch it off
+                layer_off(2);
+            } else {
+                // If not already set, then switch the layer on
+                layer_on(2);
+            }
+            break;
+	case TD_NONE: break;
+	case TD_UNKNOWN: break;
+    }
+}
+
+void fnd_reset(qk_tap_dance_state_t *state, void *user_data) {
+    // If the key was held down and now is released then switch off the layer
+    if (fnd_tap_state.state == TD_SINGLE_HOLD) {
+        layer_off(1);
+    }
+    fnd_tap_state.state = TD_NONE;
+}
+
+// Associate our tap dance key with its functionality
+qk_tap_dance_action_t tap_dance_actions[] = {
+    [FND] = ACTION_TAP_DANCE_FN_ADVANCED_TIME(NULL, fnd_finished, fnd_reset, 275)
+};
+// ------------------------------------------------------------------------------------------------
 
 bool is_alt_tab_active = false; // Flag to check if alt tab is active
 uint32_t alt_tab_timer = 0;     // Time trigger for alt tab
@@ -334,8 +415,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 				}
 				// Resetting top, mid and bottom indicators to state they were before the lock
 				led_state = host_keyboard_led_state();
-				writePin(TOP_INDICATOR_PIN, !led_state.caps_lock);
-				writePin(MID_INDICATOR_PIN, !led_state.num_lock);
+				writePin(TOP_INDICATOR_PIN, !led_state.num_lock);
+				writePin(MID_INDICATOR_PIN, !led_state.caps_lock);
 				writePin(BOT_INDICATOR_PIN, !IS_LAYER_ON(2));
 				are_leds_lit = false;
 				return false;
@@ -350,26 +431,26 @@ bool led_update_kb(led_t led_state) {
 	bool res = led_update_user(led_state);
 	if (!is_keyboard_locked){
 		if(res) {
-			writePin(TOP_INDICATOR_PIN, !led_state.caps_lock);
-			writePin(MID_INDICATOR_PIN, !led_state.num_lock);
+			writePin(TOP_INDICATOR_PIN, !led_state.num_lock);
+			writePin(MID_INDICATOR_PIN, !led_state.caps_lock);
 		}
 	}
 	return res;
 }
 
-//layer_state_t layer_state_set_user(layer_state_t state) {
-//	current_layer = get_highest_layer(state);
-//	
-//	switch (current_layer) {
-//		case 2:
-//			writePin(BOT_INDICATOR_PIN, 0);
-//			break;
-//		default: //  for any other layers, or the default layer
-//			writePin(BOT_INDICATOR_PIN, 1);
-//			break;
-//	}
-//	return state;
-//}
+layer_state_t layer_state_set_user(layer_state_t state) {
+	current_layer = get_highest_layer(state);
+	
+	switch (current_layer) {
+		case 2:
+			writePin(BOT_INDICATOR_PIN, 0);
+			break;
+		default: //  for any other layers, or the default layer
+			writePin(BOT_INDICATOR_PIN, 1);
+			break;
+	}
+	return state;
+}
 
 void housekeeping_task_user(void) { // The very important timer.
 	if (is_alt_tab_active) {
