ccfc852881490ddf1d8b30942b6aca54f59df3b1
Gondolindrim <alvaro.volpato@usp.br>
2021-09-24 01:10:08 -0300

    Adjust rules.mk

--- keyboards/mode/m65ha_alpha/rules.mk
+++ keyboards/mode/m65ha_alpha/rules.mk
@@ -6,8 +6,8 @@ MCU = STM32F401
 #
 BOOTMAGIC_ENABLE = full     # Virtual DIP switch configuration
 MOUSEKEY_ENABLE = no       # Mouse keys
-EXTRAKEY_ENABLE = no       # Audio control and System control
-CONSOLE_ENABLE = yes       # Console for debug
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no        # Commands for debug and configuration
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
@@ -21,9 +21,3 @@ EEPROM_DRIVER = i2c
 # Enter lower-power sleep mode when on the ChibiOS idle thread
 OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE -DDEBUG_EEPROM_OUTPUT=TRUE
 STM32_BOOTLOADER_ADDRESS = 0x1FFF0000
-#VIA_ENABLE=yes
-LTO_ENABLE=yes
-
-LTO_ENABLE = no
-OPT = 0
-OPT_DEFS += -g
