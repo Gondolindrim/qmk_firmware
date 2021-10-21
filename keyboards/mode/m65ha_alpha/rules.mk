1b055cf214b10d908ad1c1798d380ef22c329095
Gondolindrim <alvaro.volpato@usp.br>
2021-10-20 23:52:27 -0300

    Remove BLUETOOTH_ENABLE and use BOOTMAGIC=yes

--- keyboards/mode/m65ha_alpha/rules.mk
+++ keyboards/mode/m65ha_alpha/rules.mk
@@ -4,7 +4,7 @@ MCU = STM32F401
 # Build Options
 #   change yes to no to disable
 #
-BOOTMAGIC_ENABLE = full     # Virtual DIP switch configuration
+BOOTMAGIC_ENABLE = yes     # Virtual DIP switch configuration
 MOUSEKEY_ENABLE = no       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
@@ -15,7 +15,6 @@ SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 NKRO_ENABLE = no           # USB Nkey Rollover
 BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
-BLUETOOTH_ENABLE = no       # Enable Bluetooth
 AUDIO_ENABLE = no           # Audio output
 EEPROM_DRIVER = i2c
 # Enter lower-power sleep mode when on the ChibiOS idle thread
--- keyboards/mode/m65hi_preAlpha/rules.mk
+++ keyboards/mode/m65hi_preAlpha/rules.mk
@@ -4,10 +4,10 @@ MCU = STM32F401
 # Build Options
 #   change yes to no to disable
 #
-BOOTMAGIC_ENABLE = full     # Virtual DIP switch configuration
+BOOTMAGIC_ENABLE = yes     # Virtual DIP switch configuration
 MOUSEKEY_ENABLE = no       # Mouse keys
-EXTRAKEY_ENABLE = no       # Audio control and System control
-CONSOLE_ENABLE = yes       # Console for debug
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no        # Commands for debug and configuration
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
@@ -15,15 +15,8 @@ SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 NKRO_ENABLE = no           # USB Nkey Rollover
 BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
-BLUETOOTH_ENABLE = no       # Enable Bluetooth
 AUDIO_ENABLE = no           # Audio output
 EEPROM_DRIVER = i2c
 # Enter lower-power sleep mode when on the ChibiOS idle thread
 OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE -DDEBUG_EEPROM_OUTPUT=TRUE
 STM32_BOOTLOADER_ADDRESS = 0x1FFF0000
-#VIA_ENABLE=yes
-LTO_ENABLE=yes
-
-LTO_ENABLE = no
-OPT = 0
-OPT_DEFS += -g
