6fd04e16504a706b3c704247aca4e8803a86c03a
Gondolindrim <alvaro.volpato@usp.br>
2021-11-18 21:05:26 -0300

    Fix left spacebar keycode on VIA keymap

--- keyboards/acheron/elongate/keymaps/via/keymap.c
+++ keyboards/acheron/elongate/keymaps/via/keymap.c
@@ -14,7 +14,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include QMK_KEYBOARD_H
-#define SPC_L2
+#define SPC_L2 LT(2, KC_SPACE)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [0] = LAYOUT_all( /* Base */
