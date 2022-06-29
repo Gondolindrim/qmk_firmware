6fd28d869a2f4e01a81a595203c8c022622a7438
Gondolindrim <alvaro.augusto.volpato@gmail.com>
2022-06-29 19:07:56 -0300

    Fix column pin definition

--- keyboards/jasmine/config.h
+++ keyboards/jasmine/config.h
@@ -29,7 +29,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MATRIX_ROWS 5
 #define MATRIX_COLS 15
 //                        C0 , C1 , C2 , C3 , C4 , C5 , C6 , C7 , C8 , C9 , C10, C11, C12, C13, C14
-#define MATRIX_COL_PINS { A2 , C14, C13, B9 , B8 , B5 , B4 , B3 , A15, A10, C8 , B15, B14, B13, B12 }
+#define MATRIX_COL_PINS { A2 , C14, C13, B9 , B8 , B5 , B4 , B3 , A15, A10, A8 , B15, B14, B13, B12 }
 //                        R0 , R1 , R2 , R3 , R4
 #define MATRIX_ROW_PINS { B1 , B10, C15, A0 , A1 }
 #define DIODE_DIRECTION COL2ROW
@@ -41,4 +41,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define STM32_HSECLK 8000000
 
-#define EEPROM_I2C_24LC256
+//#define EEPROM_I2C_24LC256
