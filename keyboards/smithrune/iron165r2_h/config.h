69394d8f70b1cfdea30bb4adb786d06be09eadc6
Gondolindrim <alvaro.augusto.volpato@gmail.com>
2022-09-21 00:01:56 -0300

    Change default option to LUKE_RGBs

--- keyboards/smithrune/iron165r2_h/config.h
+++ keyboards/smithrune/iron165r2_h/config.h
@@ -52,9 +52,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_EFFECT_TWINKLE
 
 //#define ALL_RGBS // Define the RGB option here
-#define LINE_RGBS
+//#define LINE_RGBS
 //#define RUNE_RGBS
-//#define LUKE_RGBS
+#define LUKE_RGBS
 
 #define RGBLIGHT_LED_MAP {16 , 0  , 1  , 2  , 3  , 17 , 19 , 15 , 4  , 5  , 6  , 7  , 8  ,  9 , 10 , 18 , 11 , 12 , 13 , 14 , 20}
 //                        D18, D19, D20, D21, D22, D23, D24, D25, D26, D27, D28, D58, D59, D60, D61, D62, D63, D64, D65, D66, D67
