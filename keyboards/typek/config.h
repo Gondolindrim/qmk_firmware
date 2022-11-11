a2f4af58bb5300d66970a02c760f6b71ad76d896
Gondolindrim <alvaro.augusto.volpato@gmail.com>
2022-11-11 14:12:42 -0300

    Limit default brightness value due to LED number

--- keyboards/typek/config.h
+++ keyboards/typek/config.h
@@ -44,6 +44,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGB_DI_PIN B15
 #define RGBLED_NUM 67
 #define RGBLIGHT_ANIMATIONS
+#define RGBLIGHT_DEFAULT_VAL 50
 
 #define RGBLIGHT_EFFECT_BREATHING
 #define RGBLIGHT_EFFECT_RAINBOW_MOOD
