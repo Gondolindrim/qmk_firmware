bf9ffa28091cfe1308eb6a3ba629dae8cd3ffaa8
Gondolindrim <alvaro.augusto.volpato@gmail.com>
2025-05-13 21:20:57 +0200

    Remove  from LAYOUT

--- keyboards/wisp/keymaps/default/keymap.c
+++ keyboards/wisp/keymaps/default/keymap.c
@@ -16,7 +16,7 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-[0] = LAYOUT_all( /* Base */
+[0] = LAYOUT( /* Base */
     KC_MUTE, KC_MSTP, KC_MPLY, 
     KC_MUTE, KC_MPRV, KC_MNXT 
 )
